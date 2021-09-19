c865e71f71ce337baae15ade09af51038c52d30b
lukascobit <lukyn.odehnal333@gmail.com>
2021-09-19 16:29:31 +0200

    animation

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+const blockSize = 50
 var size = Math.round(window.innerHeight/150)*100
 
 function Signin() {
@@ -8,12 +9,25 @@ function Signin() {
   const draw = c => {
     c.fillStyle = "#" + ((1<<24)*Math.random() | 0).toString(16)
     for (let i = 0; i < 20; i++) {
-      c.fillRect(Math.round((Math.random()*size)/100)*100,Math.round((Math.random()*size)/100)*100, 100, 100)
+      c.fillRect(Math.round((Math.random()*size)/blockSize)*blockSize,Math.round((Math.random()*size)/blockSize)*blockSize, blockSize, blockSize)
     }
+    let isVisible = false
+    
+    var timer = setInterval(()=>{
+      for (let i = 0; i < 10; i++) {
+        let oneRand = [Math.round((Math.random()*size)/blockSize)*blockSize, Math.round((Math.random()*size)/blockSize)*blockSize]
+        isVisible ? c.fillRect(oneRand[0],oneRand[1], blockSize, blockSize) : c.clearRect(oneRand[0],oneRand[1], blockSize, blockSize)
+        isVisible = !isVisible
+      }
+      
+    },1000)
+  }
+  function send(e){
+    e.preventDefault()
+    
   }
   
   useEffect(() => {
-    
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
     
@@ -30,8 +44,11 @@ function Signin() {
         width={size}
         height={size}
       />
-      <h1>What do you see in this image?</h1>
-      <input className="whatSee" placeholder="a tree, a cloud, a house" type="text" value={body} onChange={e=>setbody(e.target.value)} />
+      <h1>What do you see in this animation?</h1>
+      <form onClick={e => send(e)} action="">
+        <input className="whatSee" placeholder="a tree, a cloud, a house" type="text" value={body} onChange={e=>setbody(e.target.value)} />
+        <button>send</button>
+      </form>
     </div>
   );
 }
